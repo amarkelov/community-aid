818a62b3f5f258053611ab73a779d9867240783f
Alex Markelov <alex@markelov.org>
2005-06-25 14:04:48 +0000

    Changes: * database login uses gmadmin instead of root * timeslot field in the db changed from varchar into time * classification added. db changed ( new tables call_mclass and call_sclass ).   class field in calls table changed into int to reflect classification change. * many repeatetive code lines isolated into functions. see functions.inc

--- www/calls.php
+++ www/calls.php
@@ -2,13 +2,15 @@
 <head>
 <?php
 
+require 'functions.inc';
+
 // global $db, $clientid, $list, $done  ;
 
 // if (!$list )  {$list="Grey" ; }
 
 $db = "gmpDb";
 //$db = $HTTP_HOST . "Db";
-$dbConnect = mysql_connect("localhost", "root", "old290174");
+$dbConnect = mysql_connect("localhost", "gmadmin", "old290174");
 if (!$dbConnect) {
 	die('Could not connect: ' . mysql_error());
 }
@@ -28,7 +30,7 @@ if ($submit and $clientid) {
 
 	$callclass = strtoupper(trim($callclass));
 	$sql  = "INSERT INTO calls (clientid,chat,class)";
-	$sql .= " VALUES ('$clientid', '$chat', '$callclass') ";
+	$sql .= " VALUES ('$clientid', '$chat', '$mclass') ";
 	$result = mysql_query($sql);
 	if (!$result) {
 		$message  = 'Invalid query: ' . mysql_error() . "\n";
@@ -37,7 +39,6 @@ if ($submit and $clientid) {
 	}
 	unset($clientid);
 }
-
 
 if ($clientid and !$submit) {
 
@@ -50,7 +51,7 @@ if ($clientid and !$submit) {
 	}
 
 	$sql = "SELECT * FROM clients WHERE (clients.clientid=$clientid)";
-	$result = mysql_query($sql);
+	$result = mysql_query($sql, $dbConnect);
 	if (!$result) {
 		$message  = 'Invalid query: ' . mysql_error() . "\n";
 		$message .= 'Whole query: ' . $query;
@@ -93,14 +94,13 @@ if ($clientid and !$submit) {
 	$description1=$myrow["description1"];
 	$description2=$myrow["description2"];
 	$done=$myrow["done"];
-
 } // if ($clientid and !$submit)
 
-	if (!$clientid and $list != 'white' and $list != 'black')   {
-		echo (' <meta http-equiv="refresh" content="12" url="http://'
-				. $HTTP_ENV_VARS["HOSTNAME"]
-				. $PHP_SELF . '?list=' . $list . '"  /> ');
-	}
+if (!$clientid and $list != 'white' and $list != 'black')   {
+	echo (' <meta http-equiv="refresh" content="12" url="http://'
+			. $HTTP_ENV_VARS["HOSTNAME"]
+			. $PHP_SELF . '?list=' . $list . '"  /> ');
+}
 
 ?>
 
@@ -119,10 +119,10 @@ function displayStatusMsg(msgStr) { //v1.0
 </head>
 
 
-<body bgcolor="#99CC99" link="#0000FF" vlink="#0000FF" alink="#0000FF">
+<body bgcolor="#BEC8FD" link="#0000FF" vlink="#0000FF" alink="#0000FF">
 <font face="Verdana, Arial, Helvetica, sans-serif" size="2">
 
-<table width="100%" border="0" cellpadding="5" bgcolor="#99cc99">
+<table width="100%" border="0" cellpadding="5">
   <tr> 
     <td   bgcolor="<?php if (($list != 'Grey') and ($list!='grey')) {echo $list; } else {echo 'gray'; } ?>" height="33">
       <div align="right">
@@ -130,24 +130,24 @@ function displayStatusMsg(msgStr) { //v1.0
   echo("
 <a href=\"$PHP_SELF?list=magenta\"><img src=\"images/magenta.png\" width=\"16\" height=\"20\" border=\"0\"></a> \n
 <a href=\"$PHP_SELF?list=red\"><img src=\"images/red.png\" width=\"16\" height=\"20\" border=\"0\"></a> \n
-  <a href=\"$PHP_SELF?list=yellow\"><img src=\"images/yellow.png\" width=\"16\" height=\"20\" border=\"0\"></a> \n
-  <a href=\"$PHP_SELF?list=green\"><img src=\"images/green.png\" width=\"16\" height=\"20\" border=\"0\"></a> \n
-  <a href=\"$PHP_SELF?list=olive\"><img src=\"images/olive.png\" width=\"16\" height=\"20\" border =\"0\"></a> \n
-  <a href=\"$PHP_SELF?list=cyan\"><img src=\"images/cyan.png\" width=\"16\" height=\"20\" border =\"0\"></a> \n
-  <a href=\"$PHP_SELF?list=blue\"><img src=\"images/blue.png\" width=\"16\" height=\"20\" border=\"0\"></a> \n
-  <a href=\"$PHP_SELF?list=grey\"><img src=\"images/grey.png\" width=\"16\" height=\"20\" border=\"0\"></a> \n
-  <a href=\"$PHP_SELF?list=white\"><img src=\"images/white.png\" width=\"16\" height=\"20\" border=\"0\"></a> \n
+<a href=\"$PHP_SELF?list=yellow\"><img src=\"images/yellow.png\" width=\"16\" height=\"20\"  border=\"0\"></a> \n
+<a href=\"$PHP_SELF?list=green\"><img src=\"images/green.png\" width=\"16\" height=\"20\"  border=\"0\"></a> \n
+<a href=\"$PHP_SELF?list=olive\"><img src=\"images/olive.png\" width=\"16\" height=\"20\"  border =\"0\"></a> \n
+<a href=\"$PHP_SELF?list=cyan\"><img src=\"images/cyan.png\" width=\"16\" height=\"20\"  border =\"0\"></a> \n
+<a href=\"$PHP_SELF?list=blue\"><img src=\"images/blue.png\" width=\"16\" height=\"20\"  border=\"0\"></a> \n
+<a href=\"$PHP_SELF?list=grey\"><img src=\"images/grey.png\" width=\"16\" height=\"20\" border=\"0\"></a> \n
+<a href=\"$PHP_SELF?list=white\"><img src=\"images/white.png\" width=\"16\" height=\"20\"  border=\"0\"></a> \n
   ");
-         }
+}
 ?>
 
-      </div>
-	</td>
-	<td width="75%" height="33">
-	<form method="post" action="<?php echo ($PHP_SELF . "?list=" . $list) ?>" >
-
+</div>
+</td>
+<td width="75%" height="33">
+<form method="post" action="<?php echo ($PHP_SELF . "?list=" . $list) ?>" >
 
 <?php
+// Client name (bold and big) when one selected
 	print('<font size="4">');
 	print("<b>");
 	printf ($firstname);
@@ -155,177 +155,122 @@ function displayStatusMsg(msgStr) { //v1.0
 	printf ($lastname);
 	print(" </b>");
 	print("</font>");
-?> 
+?>
 
-<font color="#99CC99">-</font>
-    </td>
-  </tr>
-  <tr> 
-    <td rowspan="3" width="25%" valign="top"> <font face="Verdana, Arial, Helvetica, sans-serif" size="2"> 
-      <br />
-      <?php
+</font>
+</td>
+</tr>
+<tr> 
+<td rowspan="3" width="25%" valign="top"> 
+<font face="Verdana, Arial, Helvetica, sans-serif" size="2"> 
+<br>
+<?php
+      draw_clients_list($dbConnect, $list, $clientid);
+?>
+</font> 
+</td>
 
-    // print the list 
-if ($list == "black")  {
-	$sql  = "SELECT * FROM clients where (clients.list = '$list' )";
-	$sql .= " order by lastname, firstname";
-   $result = mysql_query( $sql, $dbConnect);
-	if (!$result) {
-		$message  = 'Invalid query: ' . mysql_error() . "\n";
-		$message .= 'Whole query: ' . $query;
-		die($message);
+<td width="75%" valign="top" align="center" height="120" > 
+<font face="Verdana, Arial, Helvetica, sans-serif" size="2"> 
+</font>
+<div align="right">
+<font face="Verdana, Arial, Helvetica, sans-serif" size="2"> 
+<img src="images/new/home.png" alt="<?php echo "$houseno  $street  $postcode  -- $housing" ?>"onClick="displayStatusMsg('<?php echo  "$houseno  $street  $postcode  -- $housing" ?>');return document.returnValue"> 
+<img src="images/new/doctor.png" alt="<?php echo $gpname  ?>" onClick="displayStatusMsg('<?php echo "$gpname" ?>');return document.returnValue"> 
+<img src="images/new/phone1.png" alt="<?php echo "$contact1name, $contact1relationship  -- $contact1phone1" ?>"onClick="displayStatusMsg('<?php echo "$contact1name, $contact1relationship  -- $contact1phone1" ?>');return document.returnValue"> 
+<img src="images/new/phone2.png" alt="<?php echo "$contact2name, $contact2relationship  -- $contact2phone1" ?>" onClick="displayStatusMsg('<?php echo "$contact2name, $contact2relationship  -- $contact2phone1" ?>');return document.returnValue"> 
+<br>
+<br>
+<br>
+</font> </div>
+<div align="left">
+<font face="Verdana, Arial, Helvetica, sans-serif" size="2"> 
+
+<b>Client details:</b>
+	<?php
+	    draw_client_details($clientid, $dbConnect);
+	?>
+<br>
+<b>Report:</b>
+<br> 
+<textarea name="chat" cols="65" rows="4" wrap="PHYSICAL"></textarea>
+<br><br>
+<table width="100%">
+<tr>
+<td width="30%">
+<b>Call Classification:</b>
+</td>
+<td>
+<select name="mclass" size="1">
+
+	<?php	
+// classification	
+	    draw_classification($dbConnect);
+	?>
+	
+</select>
+</td>
+</tr>
+<tr>
+<td width="30%">
+<b>Next Call at:</b>
+</td>
+<td>
+<input type="text" name="timeslot" size="6" maxlength="5" value="<?php 
+//	echo $timeslot ===> set maxlength="5"
+	if(!empty($timeslot)) {  
+	    if ( ereg( "([0-9]{2}):([0-9]{2})", $timeslot, $regs ) ) {
+		print "$regs[1]:$regs[2]";
+	    }
+	    else {
+		print "Invalid time format: $timeslot";
+	    }
 	}
-} 
-elseif ($list == "white") {
-	$sql  = "SELECT * FROM clients where (clients.list != 'black' )";
-	$sql .= " order by done, lastname, firstname";
-   $result = mysql_query( $sql, $dbConnect);
-	if (!$result) {
-		$message  = 'Invalid query: ' . mysql_error() . "\n";
-		$message .= 'Whole query: ' . $query;
-		die($message);
-	}
-}
-else {
-	$sql  = "SELECT * FROM clients where (clients.list = '$list' )";
-	$sql .= " order by done, timeslot, firstname";
-	$result = mysql_query( $sql, $dbConnect);
-	if (!$result) {
-		$message  = 'Invalid query: ' . mysql_error() . "\n";
-		$message .= 'Whole query: ' . $query;
-		die($message);
-	}
-}
+	?>"><b><font size="1" color="#FF0000"> (24 hour format HH:MM)</font></b>
+</div>
+</td>
+</tr>
+</table>
+<br><br>
 
-while ($myrow = mysql_fetch_array($result)) {
-	$tmp_done = $myrow["done"]; 
-	$tmp_clientid = $myrow["clientid"];
-	$tmp_firstname = $myrow["firstname"]; 
-	$tmp_lastname = stripslashes($myrow["lastname"]);
+<table width="100%">
+<tr>
+<td width="30%"><center>
+<input type="submit" name="submit" value="Not Finished"
+  onClick='javascript: done.value="false";'  />
+</td>
+<td width="30%"><center>
+<input type="submit" name="submit" value="Finished"
+  onClick='javascript: done.value="true";'  />
+<td></td>
+</tr>
+</table>
 
-	if ( $clientid)  {
-		print ("$tmp_firstname  $tmp_lastname \n");
-	}
-	else {
-		$out  = "<a href=\"$PHP_SELF?clientid=$tmp_clientid\">";
-		$out .= "$tmp_firstname  $tmp_lastname</a> \n";
-		print ($out);
-	}
+<input type="hidden" name="clientid" value="<?php echo $clientid ?>">
+<input type="hidden" name="list" value="<?php echo $list ?>">
+<input type="hidden" name="done" value="<?php echo $done ?>" />
 
-	print ("<br><div align=right>");
-	//	 print($done);
-
-	if ( $tmp_done != "true")
-		print ($myrow["timeslot"]);
-
-	print ("</div>");
-}
-
-?> </font> </td>
-    <td width="75%" valign="top" align="center" height="120" > <font face="Verdana, Arial, Helvetica, sans-serif" size="2"> 
-      </font>
-        <div align="right"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"> 
-          <font color="#99CC99">-</font><img src="images/house.png" width="30" height="30" alt="<?php echo "$houseno  $street  $postcode  -- $housing" ?>"onClick="displayStatusMsg('<?php echo  "$houseno  $street  $postcode  -- $housing" ?>');return document.returnValue"> 
-          <font color="#99CC99">--</font><img src="images/medicine.png" width="30" height="30" alt="<?php echo $gpname  ?>" onClick="displayStatusMsg('<?php echo "$gpname" ?>');return document.returnValue"> 
-          <font color="#99CC99">--</font><img src="images/telephon.png" width="30" height="30" alt="<?php echo "$contact1name, $contact1relationship  -- $contact1phone1" ?>"onClick="displayStatusMsg('<?php echo "$contact1name, $contact1relationship  -- $contact1phone1" ?>');return document.returnValue"> 
-          <font color="#99CC99">--</font><img src="images/telephon.png" width="30" height="30" alt="<?php echo "$contact2name, $contact2relationship  -- $contact2phone1" ?>" onClick="displayStatusMsg('<?php echo "$contact2name, $contact2relationship  -- $contact2phone1" ?>');return document.returnValue"> 
-          <br>
-          <br>
-          <br>
-          </font> </div>
-        
-      <div align="left"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"> 
-        Phone 1: <b><?php echo $phone1 ?></b> <font color="#99CC99">----</font>
-        Phone 2: <b><?php echo $phone2 ?></b> <font color="#99CC99">--------</font>
-        D.o.b: <?php
-	if ( ereg( "([0-9]{4})-([0-9]{1,2})-([0-9]{1,2})", $dob, $regs ) ) {
-	echo "$regs[3]-$regs[2]-$regs[1]";
-		}
-		?> <br>
-        Ailments: <?php echo $ailments ?><br>
-        Notes: <b><?php echo $note ?></b><br>
-        Report: 
-        <textarea name="chat" cols="65" rows="4" wrap="PHYSICAL"></textarea>
-        <BR>
-        Next Call: 
-        <input type="text" name="timeslot" size="7" maxlength="7" value="<?php echo $timeslot  ?>">
-        <font color="#99CC99">----</font>
-	Class:
-	<input type="text" name="callclass" size="7" maxlength="7" value="<?php echo $callclass  ?>">
-        <font color="#99CC99">----</font>
-
-         <input type="submit" name="submit" value="Not Finished"
-           onClick='javascript: done.value="false";'  />
-        <font color="#99CC99">----</font>
-         <input type="submit" name="submit" value="Finished"
-           onClick='javascript: done.value="true";'  />
-        </font></div>
-
-       <input type="hidden" name="clientid" value="<?php echo $clientid ?>">
-       <input type="hidden" name="list" value="<?php echo $list ?>">
-       <input type="hidden" name="done" value="<?php echo $done ?>" />
-
-      </form> </td>
-  </tr>
-  <tr > </tr>
-  <tr>
-    <td width="75%" bgcolor="#dddddd" valign="top" align="left" height="100"><font size ="1"> 
+</form>
+</td>
+</tr>
+<tr></tr>
+<tr>
+<td width="75%" valign="top" align="left" height="100"><font size ="1"> 
 
 <?php
 
 $last_dow = 6;
 
 if($clientid) {
-	$sql  = "SELECT time,chat,class FROM calls WHERE";
-	$sql .= " (clientid= '$clientid') ORDER BY callid DESC ";
-	$result = mysql_query( $sql, $dbConnect);
-	if (!$result) {
-		$message  = 'Invalid query: ' . mysql_error() . "\n";
-		$message .= 'Whole query: ' . $query;
-		die($message);
-	}
-
-	while ($myrow = mysql_fetch_array($result)) {
-		$time=$myrow["time"];
-
-		print ('<font color="#FF0000">');
-
-		if ( ereg( "([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})", $time, $regs ) ) {
-			$this_dow = date("w",mktime(0,0,0,$regs[2],$regs[3],$regs[1]));
-
-			if ($this_dow > $last_dow)   {
-				print ('<hr />');
-			}
-
-			$last_dow = $this_dow;
-			print "$regs[3]/$regs[2]/$regs[1] $regs[4]:$regs[5]";
-		}
-		else {
-			print "Invalid date format: $time";
-		}
-
-		print(" | ");
-		print('</font>');
-		print('<font color="#0000FF">');
-		print($myrow["class"]);
-		print(" | ");
-		print('</font>');
-		print($myrow["chat"] );
-		print("<br>");
-
-	} // while
+    draw_calls( $dbConnect, $clientid);
 } // if($clientid)
 ?> 
       <p>&nbsp;</p>
       </font> </td>
   </tr>
-  <tr> 
-    <td width="25%">&nbsp;</td>
-    <td width="75%"><font face="Verdana, Arial, Helvetica, sans-serif"> </font></td>
-  </tr>
 </table>
+
 </font>
+
 </body>
-
 </html>
-
