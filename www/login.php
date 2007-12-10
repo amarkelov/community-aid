9c1478245f927b9668fa9db47f88cd78be232bfe
Alex Markelov <alex@markelov.org>
2007-12-10 00:16:38 +0000

    debug removed. migrated to PostgreSQL.

--- www/login.php
+++ www/login.php
@@ -1,52 +1,56 @@
 <?php
-	session_start();
-	require("functions.inc");
-	
-	$settings = get_ca_settings();
-	
-	if ($settings['debug'] > 0) {
-		print_debug( $clean, $settings);
-	}
-		
-	if (isset($_SESSION['s_username'])) {
-		header("Location: ".$_GET['f'] ."?a"); //already logged in
-	}else{
-		if(isset($_POST['password']))
-		{
-			if (isset($_POST['loginname'])) {
-				$loginname = $_POST['loginname'];
-				$saltypwd = salt($_POST['password']);
-			
-				$dbConnect = preLoginDBConnect();
-				
-				$query = mysql_query("SELECT operatorid,loginname,fullname,saltypwd FROM operators".
-					" WHERE loginname='$loginname'") or die("Error Logging into the Server");
-					
-				$data = mysql_fetch_array($query);
-				
-				mysql_free_result($query); 
+session_start();
+require("functions.inc");
 
-				if ( $data['saltypwd'] != $saltypwd) { 
-					$saltypwd="";
-					session_destroy();
-					sleep(3);
-					header("Location: " . $_GET['f']. "?f"); //failed login
-					exit;
-				}
-				else {
-					$_SESSION['s_username'] = $data['loginname'];
-					$_SESSION['operatorid'] = $data['operatorid'];
-					$_SESSION['fullname'] = $data['fullname'];
-				}
-				
-				dbclose( $dbConnect);
+$settings = get_ca_settings();
+
+if ($settings['debug'] > 0) {
+	print_debug( $clean, $settings);
+}
+	
+if (isset($_SESSION['s_username'])) {
+	header("Location: ".$_GET['f'] ."?a"); //already logged in
+}else{
+	if(isset($_POST['password']))
+	{
+		if (isset($_POST['loginname'])) {
+			$loginname = $_POST['loginname'];
+		
+			$dbConnect = preLoginDBConnect();
+			
+			$sql = "SELECT operatorid,loginname,fullname,saltypwd FROM operators" .
+				" WHERE loginname='" . $loginname . "'";
+			$result = pg_query( $dbConnect, $sql);
+	
+			if ( !$result) {
+				$message  = 'Invalid query: ' . pg_result_error( $result) . '<br>' . 'Query: ' . $sql;
+				die($message);
 			}
+				
+			$data = pg_fetch_array( $result);
+			
+			pg_free_result( $result); 
+
+			if ( $data['saltypwd'] !== salt($_POST['password'])) { 
+				session_destroy();
+				sleep(3);
+				header("Location: " . $_GET['f']. "?f"); //failed login
+				exit;
+			}
+			else {
+				$_SESSION['s_username'] = $data['loginname'];
+				$_SESSION['operatorid'] = $data['operatorid'];
+				$_SESSION['fullname'] = $data['fullname'];
+			}
+			
+			dbclose( $dbConnect);
 		}
 	}
-	if (isset($_GET['f']) ){
-		header("Location: ".$_GET['f']);  // return to page we came from
-		
-	}else{
-		header("Location: " . $settings['start_page']); // dont know where to go!
-	} 
+}
+if (isset($_GET['f']) ){
+	header("Location: ".$_GET['f']);  // return to page we came from
+	
+}else{
+	header("Location: " . $settings['start_page']); // dont know where to go!
+} 
 ?>   
