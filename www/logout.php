84bf88d76e8279506866f5db6094fce2f437df0b
Alex Markelov <alex@markelov.org>
2007-12-17 01:47:22 +0000

    minor formating change

--- www/logout.php
+++ www/logout.php
@@ -1,8 +1,8 @@
 <?php
-	require 'functions.inc';
-	session_start();
-	$_SESSION = array();
-	session_destroy();
-	$settings = get_ca_settings();
-	header("Location: " . $settings['start_page']);
+require 'functions.inc';
+session_start();
+$_SESSION = array();
+session_destroy();
+$settings = get_ca_settings();
+header("Location: " . $settings['start_page']);
 ?>
