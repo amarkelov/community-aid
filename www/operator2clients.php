178dbf8e13d8fcf390551f97eb3fe502d67063bd
Alex Markelov <alex@markelov.org>
2012-06-28 09:34:27 +0000

    Added multiple group assignment per operator

--- www/operator2clients.php
+++ www/operator2clients.php
@@ -9,7 +9,7 @@ $clean = array();
 $settings = get_ca_settings();
 
 // Page Header ...
-printHeader( "Assign Operator to Client", 0);
+printHeader( "Assign Operator to Group of clients", 0);
 
 // if debug flag is set, print the following info
 if ($settings['debug'] > 0) {
@@ -31,29 +31,11 @@ if($doWeExit == true){
 if(isset($_POST['submit'])) {
 	$clean['submit'] = $_POST['submit'];
 }
-else if(isset($_POST['edit'])) {
-	$clean['edit'] = $_POST['edit'];
-}
-else if(isset($_POST['group'])) {
-	$clean['group'] = $_POST['group'];
-}
 
 if(isset($_POST['operatorid_edit'])){
 	if(ctype_digit($_POST['operatorid_edit'])) {
 		$clean['operatorid_edit'] = $_POST['operatorid_edit'];
 	} 
-}
-if(isset($_POST['unassigned_only'])) {
-	if(isset($_POST['unassigned_only'])) {
-		switch(strtoupper($_POST['unassigned_only'])) {
-			case "ON":
-				$clean['unassigned_only'] = 1;
-				break;
-			default:
-				$clean['unassigned_only'] = 0;
-				break;
-		}
-	}
 }
 
 if(isset($_SESSION['operatorid'])){
@@ -62,15 +44,13 @@ if(isset($_SESSION['operatorid'])){
 	}
 }
 
-if(isset($_POST['districtid'])){
-	if(ctype_digit($_POST['districtid'])) {
-		$clean['districtid'] = $_POST['districtid'];
-	}
-}
-
 if(isset($_POST['groupid'])){
-	if(ctype_digit($_POST['groupid'])) {
-		$clean['groupid'] = $_POST['groupid'];
+	$clean['groupid'] = array();
+	
+	foreach($_POST['groupid'] as $id => $gid) {
+		if(ctype_digit($gid)) {
+			$clean['groupid'][] = $gid;
+		}
 	}
 }
 
@@ -83,36 +63,17 @@ if($settings['debug'] == 1){
  * End of filtering input
  */
  
-if ($clean['submit']) {
-	if ($clean['operatorid_edit']) {
-		$assigned = array();
-		
-		//assign_client( $clean['operatorid'], ...);
-		if( is_array($_POST['assigned'])) {
-			$assigned = $_POST['assigned'];
-		}
-		
-		if( addClientToOperator( $assigned, $clean['operatorid_edit'])) {
-			printMessage( 'Clients assigned to operator!');
-		}
-		else {
-			printErrorMessage( 'Error occured while assigning client to operator');
-		}
-		
-		printMessage('<a href="' . $_SERVER['PHP_SELF'] . '">Assign client(s) to another operator</a>');
-	}
-}
-else if( $clean['group']) {
+if( $clean['submit']) {
 	if ( $clean['operatorid_edit']) {
 		if ( isset( $clean['groupid'])) {
 			if( addGroupToOperator( $clean['groupid'], $clean['operatorid_edit'])) {
-				printMessage( 'Clients assigned to operator!');
+				printMessage( 'Group(s) assigned to operator!');
 			}
 			else {
-				printErrorMessage( 'Error occured while assigning client to operator');
+				printErrorMessage( 'Error occured while assigning group(s) to operator');
 			}
 			
-			printMessage('<a href="' . $_SERVER['PHP_SELF'] . '">Assign client(s) to another operator</a>');
+			printMessage('<a href="' . $_SERVER['PHP_SELF'] . '">Assign group(s) to another operator</a>');
 		}
 	}
 }
@@ -120,12 +81,11 @@ else {	// this part happens if we don't press submit
 	if ( $clean['operatorid']) {
 		// pull the list of operators
 		$operators = array();
-		$arDistricts = array();
 
 		if( getOperators( $operators)) {
 			print '<font face="Verdana, Arial, Helvetica, sans-serif" size="2">
 					<form method="post" action="' . $_SERVER['PHP_SELF'] . '">
-					<div align="left">';
+					<div align="left" style="vertical-align:top">';
 
 			print 'Choose the Operator you want to assign clients to<br><br>
 				Operator: <select name="operatorid_edit">';
@@ -137,13 +97,13 @@ else {	// this part happens if we don't press submit
 			
 			}
 			print '</select><br><br>';
-			print 'Choose the group of clients you want to assign to the operator<br><br>
+			print 'Choose the group of clients you want to assign to the operator (multiple choice is available)<br><br>
 				Group: ';
 
 			/* all groups except 'N/A' and 'Floating list' */
-			getGroupNamesAsDropDownList( "groupid", true);
+			getGroupNamesAsDropDownList( "groupid[]", true);
 			
-			print '<br><br><input type="Submit" name="group" value="Assign the Group to the Operator">
+			print '<br><br><input type="Submit" name="submit" value="Assign the Group(s) to the Operator">
 			    	</div></form></font>';
 		}
 	}
