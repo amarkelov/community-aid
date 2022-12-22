80976d404b4ac3a890759454c4a8fb38526bb568
Alex Markelov <alex@markelov.org>
2022-12-22 11:20:34 +0000

    Fix Open redirect issue in login.php
    One more check is required - the URL itself
    to make sure that we are not redirected out of
    the system to some unknown location.

--- www/login.php
+++ www/login.php
@@ -7,7 +7,7 @@ $settings = get_ca_settings();
 $clean = array();
 
 if(isset($_POST['password'])) {
-	$clean['password'] = $_POST['password']; 
+	$clean['password'] = $_POST['password'];
 }
 
 if (isset($_POST['loginname'])) {
@@ -18,16 +18,18 @@ if ($settings['debug'] > 0) {
 	require_once("functions.inc");
 	print_debug( $clean, $settings);
 }
-	
-if (isset($_SESSION['s_username'])) {
-	header("Location: ".$_GET['f'] ."?a"); //already logged in
-}else{
-	login( $clean['loginname'], $clean['password']);
+
+// if set $_GET['f'] has the page URL the user came from
+if(isset($_GET['f']) && filter_var(_GET['f'], FILTER_SANITIZE_URL)) {
+    $return_to = filter_var(_GET['f'],  FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED|FILTER_FLAG_HOST_REQUIRED);
+    if($return_to && isset($_SESSION['s_username'])) {
+        header("Location: " . $return_to);  // return to page we came from
+    }
+    else {
+        login($clean['loginname'], $clean['password']);
+    }
 }
-if (isset($_GET['f']) ){
-	header("Location: ".$_GET['f']);  // return to page we came from
-	
-}else{
-	header("Location: " . $settings['start_page']); // dont know where to go!
-} 
-?>   
+else {
+    header("Location: " . $settings['start_page']); // dont know where to go!
+}
+?>
