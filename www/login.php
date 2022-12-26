a9ab40c47a1378730b0af55e154e4fb0fe76e2f7
Alex Markelov <alex@markelov.org>
2022-12-26 22:11:24 +0000

    Fix Open redirect issue in login.php
    Check hostname is the URL and destroy the session,
    if it doesn't match our server's.

--- www/login.php
+++ www/login.php
@@ -21,15 +21,22 @@ if ($settings['debug'] > 0) {
 
 // if set $_GET['f'] has the page URL the user came from
 if(isset($_GET['f']) && filter_var(_GET['f'], FILTER_SANITIZE_URL)) {
-    $return_to = filter_var(_GET['f'],  FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED|FILTER_FLAG_HOST_REQUIRED);
-    if($return_to && isset($_SESSION['s_username'])) {
-        header("Location: " . $return_to);  // return to page we came from
+    $return_to = filter_var(_GET['f'], FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED|FILTER_FLAG_HOST_REQUIRED);
+    $hostname = parse_url($return_to, PHP_URL_HOST);
+
+    if($hostname == $settings['server_name']) {
+        if($return_to && isset($_SESSION['s_username'])) {
+            header("Location: " . $return_to);  // return to page we came from
+        }
+        else {
+            login($clean['loginname'], $clean['password']);
+        }
     }
     else {
-        login($clean['loginname'], $clean['password']);
+        session_destroy();
+        printErrorMessage('CRITICAL: URL manipulation detected!');
     }
 }
-else {
-    header("Location: " . $settings['start_page']); // dont know where to go!
-}
+
+header("Location: " . $settings['start_page']); // re-direct to default start page
 ?>
